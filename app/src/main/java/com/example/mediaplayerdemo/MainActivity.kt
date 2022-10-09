6c8931e52caf24019503aeb74df74059c3ea24d0
Masatoshi Takada <masatech517@gmail.com>
2022-10-09 17:50:56 +0900

    Initial Setup

---
+++ app/src/main/java/com/example/mediaplayerdemo/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mediaplayerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mediaplayerdemo.ui.theme.MediaPlayerDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MediaPlayerDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MediaPlayerDemoTheme {
+        Greeting("Android")
+    }
+}
